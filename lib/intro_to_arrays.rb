27ff42e56f08722db6dd5681dfd4d8be6cbd78a8
Adrian Popan <adrian.popan@gmail.com>
2019-06-12 00:12:26 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -25,3 +25,8 @@ def first_element_with_array_methods
   array = ["element1", "element2", "element3"]
   array.first
 end
+
+def last_element_with_array_methods
+  array = ["element1", "element2", "element3"]
+  array.last 
+end
